9cc33e582bccac025fcd2c6ea1bb3e56d194ac4a
Lauren <lauren.main28@gmail.com>
2023-03-25 15:27:14 -0700

    first test passing

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,7 +1,9 @@
 #from madlib_cli.madlib import read_template, parse_template, merge
 
-def read_template():
-    pass
+def read_template(txt_file):
+    with open(txt_file, 'r') as f:
+        contents = f.read()
+        return contents
 
 def parse_template():
     pass
