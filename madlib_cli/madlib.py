6fc15655454c054ae296bf371e43065341c01cae
Lauren <lauren.main28@gmail.com>
2023-03-25 15:35:52 -0700

    adds welcome message

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,16 @@
 #from madlib_cli.madlib import read_template, parse_template, merge
 
+def welcome():
+    print('''
+*********************************
+Welcome to MadLibs! 
+To play, please enter the 
+correct word type when prompted.
+Enter quit to exit.
+*********************************
+''')
+
+
 def read_template(txt_file):
     with open(txt_file, 'r') as f:
         contents = f.read()
@@ -10,3 +21,5 @@ def parse_template():
 
 def merge():
     pass
+
+welcome()
